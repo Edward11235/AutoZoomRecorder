b00d674b2cb4cfb9405ad1db25ebacc63f81b06d
Chunlin_Li <2744355699@qq.com>
2020-09-26 08:20:24 -0400

    GUI is fixed now. Hurray!

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen
 from pynput import mouse
 
 Builder.load_file("my.kv")
+
 
 def on_click(x, y, button, pressed):
     if button == mouse.Button.left:
@@ -15,20 +17,19 @@ def on_click(x, y, button, pressed):
     if not pressed:
         return False # Stop listener
 
-class MyGrid(Widget):  #MyGrid首先继承了GridLayout.在其中又声明了一个新的GridLayout对象
-    name = ObjectProperty(None)  #when nothing is read, use None
-    email = ObjectProperty(None)
+class MainWindow(Screen):
+    zoomid = ObjectProperty(None)
+    zoompassword = ObjectProperty(None)
+    meetingid = ObjectProperty(None)
 
-    def btn(self):
-        #print(pyautogui.position())
+    def position(self):
         listener = mouse.Listener(on_click=on_click)
         listener.start()
         listener.join()
-        print("Name:", self.name.text, "email:", self.email.text,  )
 
 class AutoZoomRecorderApp(App):
     def build(self):
-        return FloatLayout()
+        return MainWindow()
 
 if __name__ == "__main__":
     AutoZoomRecorderApp().run()
