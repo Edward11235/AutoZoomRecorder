f82e1c22d8c585999997264018191039c0bf52fc
Chunlin_Li <2744355699@qq.com>
2020-09-26 08:49:43 -0400

    Every button can get position and stores it in temp_x and temp_y. Time to add database.

--- main.py
+++ main.py
@@ -11,23 +11,27 @@ import pyautogui
 Builder.load_file("my.kv")
 
 
-def on_click(x, y, button, pressed):
-    if button == mouse.Button.left:
-        print('{} at {}'.format('Pressed Left Click' if pressed else 'Released Left Click', (x, y)))
-    else:
-        print('{} at {}'.format('Pressed Right Click' if pressed else 'Released Right Click', (x, y)))
-    if not pressed:
-        return False # Stop listener
-
 class MainWindow(Screen):
     zoomid = ObjectProperty(None)
     zoompassword = ObjectProperty(None)
     meetingid = ObjectProperty(None)
+    temp_x = temp_y = -1.0
+
+    def on_click(self, x, y, button, pressed):
+        if button == mouse.Button.left:
+            print('{} at {}'.format('Pressed Left Click' if pressed else 'Released Left Click', (x, y)))
+            self.temp_x = float(x)
+            self.temp_x = float(x)
+        else:
+            print('{} at {}'.format('Pressed Right Click' if pressed else 'Released Right Click', (x, y)))
+        if not pressed:
+            return False  # Stop listener
 
     def add_position(self):
-        listener = mouse.Listener(on_click=on_click)
+        listener = mouse.Listener(on_click=self.on_click)
         listener.start()
         listener.join()
+        # 把temp_x和temp_y存入数据库中
 
 
 class AutoZoomRecorderApp(App):
