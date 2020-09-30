ec92946c9c690430b21c854820609406517dcb96
Chunlin_Li <2744355699@qq.com>
2020-09-30 12:10:48 -0400

    Start button's initialize method and time related staff is ready

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from kivy.uix.screenmanager import Screen
 from pynput import mouse
 import pyautogui
 from database import DataBase
+import datetime
+from kivy.clock import Clock
+from functools import partial
 
 
 Builder.load_file("my.kv")
@@ -16,6 +19,11 @@ class MainWindow(Screen):
     zoomid = ObjectProperty(None)
     zoompassword = ObjectProperty(None)
     meetingid = ObjectProperty(None)
+    start_time = ObjectProperty(None)
+    end_time = ObjectProperty(None)
+    test = ObjectProperty(None)
+    startapp = ObjectProperty(None)
+    time_to_start = duration = -1
     temp_x = temp_y = -1.0  #用于存储读取的点击位置的坐标
 
     def on_click(self, x, y, button, pressed):
@@ -36,6 +44,50 @@ class MainWindow(Screen):
         if isinstance(self.temp_x, float) and isinstance(self.temp_x, float) and isinstance(btn_name, str):
             db.add_pos(btn_name, self.temp_x, self.temp_y)
 
+    # def test(self):
+    #     self.start(start=now+60)
+
+    def initialize(self):
+        try:
+            a, b = self.start_time.text.split(":")
+            c, d = self.end_time.text.split(":")
+            self.time_to_start = (int(a) - datetime.datetime.now().hour) * 60 + int(b) - datetime.datetime.now().minute
+            self.duration = (int(c) - int(a))*60 + int(d) - int(b)
+            if self.time_to_start < 0 or self.duration < 0:
+                raise Exception()
+            if self.duration == -1 or self.time_to_start == -1:
+                raise Exception()
+        except:
+            #popup
+            print("时间格式错误")
+        if self.duration > 60:
+            #popup
+            print("popup:超出时间上限（60分钟)")
+        elif self.time_to_start > 0 and self.duration > 0:
+            temp_hour = self.time_to_start // 60
+            temp_min = self.time_to_start % 60
+            self.startapp.text = "Testing is going to start in %dhour %dmin" % (temp_hour, temp_min)
+            self.countDown()
+
+
+    def countDown(self):
+
+        def update_label(*largs):
+            temp_hour = self.time_to_start // 60
+            temp_min = self.time_to_start % 60
+            self.startapp.text = "Testing is going to start in %dhour %dmin" % (temp_hour, temp_min)
+            self.time_to_start -= 1
+            if self.time_to_start == 0:
+                self.start()
+                self.func_interval.cancel()
+
+        self.func_interval = Clock.schedule_interval(partial(update_label), 60)
+
+    def testDountDonw(self):
+        pass
+
+    def start(self):
+        print("1")
 
 db = DataBase("info.txt")
 userdb = DataBase("user.txt")
